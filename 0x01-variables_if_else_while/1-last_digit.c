2f7451351555a2b0cc6e1e0da625f15f954c1e8d
Carlo Ertola <info@carloertola.com>
2021-02-04 09:13:00 +0000

    C code to find the last digit of a random int and display a message based on its value

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,7 +13,7 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	last_digit = n % 10;
+	last_d = n % 10;
 	if (last_d > 5)
 		printf("Last digit of %d is %d and is greater than 5\n", n, last_d);
 	else if (last_d == 0)
