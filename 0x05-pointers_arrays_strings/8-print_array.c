bb8ce54ea1a259984096b514a12cf7e29b464884
Carlo Ertola <info@carloertola.com>
2021-02-15 21:37:15 +0000

    function to print the first n elements of an int array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_array - prints first n elements of array
+ * @a: int array to display from
+ * @n: number of elements to display
+ *
+ * Return: void
+ */
+void print_array(int *a, int n)
+{
+	int i = 1;
+
+	printf("%d", *(a + 0));
+	while (i < n)
+	{
+		printf(", %d", *(a + i));
+		i++;
+	}
+	printf("\n");
+}
