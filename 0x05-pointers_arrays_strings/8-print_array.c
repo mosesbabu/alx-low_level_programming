3e2cb491f647a8b29252af0adce85ffba3323cf5
Carlo Ertola <info@carloertola.com>
2021-02-17 09:23:40 +0000

    function to print n elements of an array followed by a new line

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -9,15 +9,14 @@
  */
 void print_array(int *a, int n)
 {
-	int i = 1;
-	if (n > 0)
+	int i = 0;
+
+	while (i < n)
 	{
-		printf("%d", *(a + 0));
-		while (i < n)
-		{
-			printf(", %d", *(a + i));
-			i++;
-		}
-		printf("\n");
+		if (i == n - 1)
+			printf("%d", *(a + i));
+		else
+			printf("%d, ", *(a + i));
 	}
+	printf("\n");
 }
