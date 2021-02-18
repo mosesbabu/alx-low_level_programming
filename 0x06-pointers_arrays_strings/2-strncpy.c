1bac326e719897aac35a83c5517a1fc73439b1fe
Carlo Ertola <carloertola@gmail.com>
2021-02-18 20:01:22 +0000

    function to copy n elements of one string to another

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,11 +10,17 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i;
+	int i, j;
 
-	for (i = 0; i < n && src[i] != '\0'; i++)
+	for (i = 0; i < n; i++)
+	{
+		if (src[i] == '\0')
+		{
+			for (j = i; j < n && dest[j] != '\0'; j++)
+				dest[j] = '\0';
+			break;
+		}
 		dest[i] = src[i];
-	while (i < n)
-		dest[i++] = '\0';
+	}
 	return (dest);
 }
