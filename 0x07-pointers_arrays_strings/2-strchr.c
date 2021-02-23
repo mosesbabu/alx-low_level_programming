e84b724f27f4d446190a244b49fe3ca218c73438
Carlo Ertola <carloertola@gmail.com>
2021-02-23 07:42:08 +0000

    function that locates first occurence of a character in a string or NULL if the character is not found

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,14 +9,11 @@
  */
 char *_strchr(char *s, char c)
 {
-	int i;
-
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		if (s[i] == c)
+	do {
+		if (*s == c)
 		{
-			return (s + i);
+			return (s);
 		}
-	}
+	} while (*s++);
 	return (NULL);
 }
