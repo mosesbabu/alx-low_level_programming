e28641a5c25a24e236b88087005868d33e65701c
Carlo Ertola <carloertola@gmail.com>
2021-02-24 20:55:37 +0000

    function that compares two strings and returns 1 if the strings can be considered identical, otherwise return 0

---
+++ 0x08-recursion/100-wildcmp.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * wildcmp - returns 1 if strings match and 0 if not
+ *
+ * @s1: string 1
+ * @s2: string 2
+ * Return: int
+ */
+int wildcmp(char *s1, char *s2)
+{
+}
